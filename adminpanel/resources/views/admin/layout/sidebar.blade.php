4eb3662c36f896fa0c18ecef56c67fa1c1ca1250
Sumon Mitra <sumonmitrasm@gmail.com>
2026-08-29 10:17:20 +0600

    Datatable

--- adminpanel/resources/views/admin/layout/sidebar.blade.php
+++ adminpanel/resources/views/admin/layout/sidebar.blade.php
@@ -22,6 +22,7 @@
                             $canManageCategories = $admin?->hasModuleAccess('category', 'view');
                             $canManageSettings = $admin?->hasModuleAccess('setting', 'view');
                             $canManageTags = $admin?->hasModuleAccess('tag', 'view');
+                            $canManageNewsPosts = $admin?->hasModuleAccess('post', 'view');
 
                             // Show the Online Polling menu only when the admin has poll view access.
                             $canOnlinePolling = $admin?->hasModuleAccess('poll', 'view');
@@ -87,12 +88,12 @@
                             <path d="M11 4H4a2 2 0 0 0-2 2v14a2 2 0 0 0 2 2h14a2 2 0 0 0 2-2v-7"></path>
                             <path d="M18.5 2.5a2.121 2.121 0 0 1 3 3L12 15l-4 1 1-4 9.5-9.5z"></path>
                         </svg>
-                        <span class="side-menu__label">Forms</span><i class="angle fe fe-chevron-right"></i></a>
+                        <span class="side-menu__label">News Post</span><i class="angle fe fe-chevron-right"></i></a>
                     <ul class="slide-menu">
                         <li class="side-menu-label1">
-                            <a href="javascript:void(0)">Forms</a>
+                            <a href="javascript:void(0)">Post</a>
                         </li>
-                        <li><a href="form-elements.html" class="slide-item"> Form Elements</a></li>
+                        <li><a href="{{ route('posts') }}" class="slide-item"> NewsPost</a></li>
                     </ul>
                 </li>
 
---
+++ adminpanel/resources/views/admin/post/post.blade.php
@@ -0,0 +1,5 @@
+@extends('admin.layout.layout')
+
+@section('content')
+    @include('admin.post.partials.get-post')
+@endsection
